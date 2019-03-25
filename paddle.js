f2a3e3cc02d8f6ef2bec5c2cdbd1d16128a40242
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-25 22:57:07 +0000

    create paddle class

--- paddle.js
+++ paddle.js
@@ -1,2 +1,10 @@
 //create up and down movement for each paddle
 //up and down buttons should be params
+
+function Paddle(x,y) {
+  this.x = x;
+  this.y = y;
+  this.yv = 0;
+  this.height = 30;
+  this.width = 6;
+}
