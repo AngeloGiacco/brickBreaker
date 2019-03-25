19e0c8ee30ee01b423c4ce62f431ac1f2a7c1a8d
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-25 22:53:01 +0000

    create draw and setup functions

--- sketch.js
+++ sketch.js
@@ -1,3 +1,16 @@
 //show the ball and paddle
 //update scores
 //create two paddles with different button params
+var ball;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  ball = new Ball();
+}
+
+function draw() {
+  background(0);
+  ball.show();
+  ball.update();
+  ball.bounce();
+}
