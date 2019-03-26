2962a4ad23652061087ad3f6e7176305cfb43b25
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-26 08:39:58 +0000

    show paddle and create velocity functions

--- paddle.js
+++ paddle.js
@@ -5,6 +5,23 @@ function Paddle(x,y) {
   this.x = x;
   this.y = y;
   this.yv = 0;
-  this.height = 30;
-  this.width = 6;
+  this.h = 300;
+  this.w = 30;
+
+  this.show = function() {
+    fill(255);
+    rect(this.x - this.w/2,this.y - this.h/2,this.w,this.h);
+  }
+
+  this.moveUp = function() {
+    this.yv -= 1;
+  }
+
+  this.moveDown = function() {
+    this.yv += 1;
+  }
+
+  this.update = function() {
+    this.y += this.yv;
+  }
 }
