e949287d3773f10dedfd91ebfaf9fcf8c08cff31
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-26 11:11:24 +0000

    create wall class

---
+++ wall.js
@@ -0,0 +1,16 @@
+function Wall(x,y,width,height,level) {
+
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height:
+  this.level = level;
+
+  this.reduceLevel = function() {
+    this.levels = (this.level > 0) ? this.level - 1 : this.level;
+  }
+
+  this.show = function() {
+    rect(this.x, this.y, this.width, this.height);
+  }
+}
